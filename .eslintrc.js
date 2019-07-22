f24154094f622c46c47a328adb08be145b3c8ab5
TaeSang Cho <gsts007@gmail.com>
2019-07-22 20:28:03 +0900

    Remove linebreak-style from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
     },
     'rules': {
         'indent': [ 'error', 4 ],
-        'linebreak-style': [ 'error', 'unix' ],
         'quotes': [ 'error', 'single' ],
         'semi': [ 'error', 'always' ],
         'array-bracket-newline': [ 'error', 'consistent' ],
