46dbd225f297b866c9ee5d22b98381b1607440aa
lkkikkil <skattr13@naver.com>
2019-07-23 22:21:11 +0900

    Implements care package tooltip

--- src/components/care-package.js
+++ src/components/care-package.js
@@ -1,4 +1,4 @@
-import { Sprite, Texture } from 'pixi.js';
+import { Sprite, Texture, Text, Graphics } from 'pixi.js';
 import Component from './component';
 import { Icon } from './../assets';
 
@@ -18,11 +18,51 @@ class CarePackage extends Component {
 
         this.addChild(sprite);
 
-        this.scale.x = .2;
-        this.scale.y = .2;
+        sprite.scale.x = .2;
+        sprite.scale.y = .2;
 
         this.spawnTime = data.spawnTime;
         this.landTime = data.landTime;
+
+        this.interactive = true;
+        this.buttonMode = true;
+
+        let items = '';
+        for (let numItem = 0; numItem < data.items.length; numItem++) {
+            items += data.items[numItem].itemId;
+            if (numItem !== data.items.length - 1) {
+                items += '\n';
+            }
+        }
+
+        let showName = false;
+
+        let itemNameText = new Text(items, { fontSize: 16, fill: 'white', align: 'center' });
+        itemNameText.anchor.set(0.5, 0.5);
+        itemNameText.position.set(0, -(itemNameText.height / 2));
+
+        let itemNameBox = new Graphics();
+        itemNameBox.lineStyle(0.8, 0xffffff, 0.7);
+        itemNameBox.beginFill(0x000000, 0.6);
+        itemNameBox.drawRoundedRect(-(itemNameText.width / 2 + 2), -(itemNameText.height), itemNameText.width + 4, itemNameText.height + 4, 5);
+        itemNameBox.endFill();
+
+        itemNameText.visible = false;
+        itemNameBox.visible = false;
+
+        this.addChild(itemNameBox);
+        this.addChild(itemNameText);
+
+        this.on('click', () => {
+            showName = !showName;
+            itemNameText.visible = showName;
+            itemNameBox.visible = showName;
+        });
+
+
+        this.on('click', () => {
+
+        });
     }
 
     seek(time) {
