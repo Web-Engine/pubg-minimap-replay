56b78d4adab85529e5d1baf9432fee563ae04be9
TaeSang Cho <gsts007@gmail.com>
2019-07-30 16:52:48 +0900

    Implement component

---
+++ src/component.js
@@ -0,0 +1,26 @@
+import { Container } from 'pixi.js';
+import { calcValueRatio } from 'utils';
+import TimeData from 'time-data';
+
+class Component extends Container {
+    constructor(locations) {
+        super();
+
+        this._locations = new TimeData(locations);
+    }
+
+    update(elapsedTime) {
+        this._locations.seek(elapsedTime);
+
+        let { before, after, ratio } = this._locations;
+        let { x: beforeX, y: beforeY } = before;
+        let { x: afterX, y: afterY } = after;
+
+        let x = calcValueRatio(beforeX, afterX, ratio);
+        let y = calcValueRatio(beforeY, afterY, ratio);
+
+        this.position.set(x, y);
+    }
+}
+
+export default Component;
